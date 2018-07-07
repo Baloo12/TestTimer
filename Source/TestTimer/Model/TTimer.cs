1d8a185754a1299aeb99aaf5a8e12501b67407a2
Baloo12 <gibson12ant@gmail.com>
2018-07-07 15:46:35 +0300

    Initial commit

---
+++ Source/TestTimer/Model/TTimer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTimer.Model
+{
+    public class TTimer
+    {
+        #region Fields
+
+        private int _totalSeconds;
+        private readonly int _maxSecondsValue = 24*60*60;
+
+        #endregion
+
+        #region Constructors
+
+        public TTimer() { }
+
+        public TTimer(int maxSecondsValue)
+        {
+            _maxSecondsValue = maxSecondsValue;
+        }
+
+        #endregion
+
+        #region Properties
+        public int TotalSeconds
+        {
+            get { return _totalSeconds; }
+            private set
+            {
+                if (0 <= value && value <= _maxSecondsValue)
+                    _totalSeconds = value;
+                else
+                {
+                    if (0 < value) _totalSeconds = 0;
+                    if (value > _maxSecondsValue) _totalSeconds = _maxSecondsValue;
+                    throw new InvalidOperationException(string.Format("Value is out of range. Total seconds number should be between 0 and {0}", _maxSecondsValue));
+                }
+            }
+        }
+
+        public DateTime Time
+        {
+            get { return DateTime.MinValue + TimeSpan.FromSeconds(_totalSeconds); }
+        }
+
+        public string TimeString
+        {
+            get { return Time.ToString("HH:mm:ss"); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Tick(int step)
+        {
+            try
+            {
+                TotalSeconds += step;
+            }
+            catch (InvalidOperationException) { }
+        }
+
+        public void SetTime(int seconds, bool throwException = false)
+        {
+            try
+            {
+                TotalSeconds = seconds;
+            }
+            catch (InvalidOperationException)
+            {
+                if (throwException)
+                    throw;
+            }
+        }
+
+        public void SetTime(string secondsString, bool throwException = false)
+        {
+            int seconds = 0;
+            if (int.TryParse(secondsString, out seconds))
+            {
+                try
+                {
+                    TotalSeconds = seconds;
+                }
+                catch (InvalidOperationException)
+                {
+                    if (throwException)
+                        throw;
+                }
+            }
+            else
+                throw new InvalidCastException("Invalid time format. Please enter number of seconds between 0 and 72000");
+        }
+
+        #endregion
+    }
+}
---
+++ Source/TestTimer/ViewModel/TimerVM.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
+using TestTimer.Model;
+using System.Windows;
+
+namespace TestTimer.ViewModel
+{
+    public class TimerVM : INotifyPropertyChanged
+    {
+        #region Base
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Fields
+
+        private TTimer _inputTTimer;
+        private TTimer _tickTTimer;
+
+        private string _inputTimerContent;
+
+        private ICommand _lostFocusCommand;
+        private ICommand _gotFocusCommand;
+
+        private bool _editorMode = false;
+        private bool _oddSecondTick = true;
+
+        // Only one timer has been used
+        private static DispatcherTimer _changeValuesTimer;
+
+        private string _warrningMessage;
+        #endregion
+
+        #region Construstors
+
+        public TimerVM()
+        {
+            _inputTTimer = new TTimer(72000);
+            _tickTTimer = new TTimer();
+
+            LostFocusCommand = new RelayCommand(_ => LostFocus());
+            GotFocusCommand = new RelayCommand(_ => GotFocus());
+
+            _changeValuesTimer = new DispatcherTimer();
+            _changeValuesTimer.Interval = TimeSpan.FromSeconds(1);
+            _changeValuesTimer.Tick += (sender, e) => OnTimerTick();
+            _changeValuesTimer.Start();
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string InputTimerContent
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_warrningMessage))
+                    _inputTimerContent = _editorMode ? _inputTTimer.TotalSeconds.ToString() : _inputTTimer.TimeString;
+                return _inputTimerContent;
+            }
+            set
+            {
+                _inputTimerContent = value;
+                WarrningMessage = string.Empty;
+                try
+                {
+                    _inputTTimer.SetTime(value, true);
+                }
+                catch (Exception ex)
+                {
+                    if (ex is InvalidCastException || ex is InvalidOperationException)
+                    {
+                        WarrningMessage = ex.Message;
+                    }
+                    else
+                        throw;
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public string OddTimerContent
+        {
+            get { return _tickTTimer.TimeString; }
+            set { OnPropertyChanged(); }
+        }
+
+        public string EvenTimerContent
+        {
+            get { return _tickTTimer.TimeString; }
+            set { OnPropertyChanged(); }
+        }
+
+        #region Warnings
+
+        public string WarrningMessage
+        {
+            get { return string.Format("Warrning: {0}", _warrningMessage); }
+            set
+            {
+                _warrningMessage = value;
+                OnPropertyChanged();
+                OnPropertyChanged("IsWarrningVisible");
+            }
+        }
+
+        public Visibility IsWarrningVisible
+        {
+            get { return string.IsNullOrEmpty(_warrningMessage) ? Visibility.Collapsed : Visibility.Visible; }
+            set { OnPropertyChanged(); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand LostFocusCommand
+        {
+            get { return _lostFocusCommand; }
+            set { _lostFocusCommand = value; }
+        }
+
+        public ICommand GotFocusCommand
+        {
+            get { return _gotFocusCommand; }
+            set { _gotFocusCommand = value; }
+        }
+
+        #endregion
+        #endregion
+
+        #region Methods
+
+        private void LostFocus()
+        {
+            _editorMode = false;
+            OnPropertyChanged("InputTimerContent");
+        }
+
+        private void GotFocus()
+        {
+            _editorMode = true;
+            OnPropertyChanged("InputTimerContent");
+        }
+
+        private void OnTimerTick()
+        {
+            int offset = _editorMode ? -1 : 1;
+
+            _tickTTimer.Tick(offset);
+
+            if (!_editorMode)
+            {
+                _inputTTimer.Tick(-1);
+                OnPropertyChanged("InputTimerContent");
+            }
+
+
+            if (_oddSecondTick)
+                OnPropertyChanged("OddTimerContent");
+            else
+                OnPropertyChanged("EvenTimerContent");
+            _oddSecondTick = !_oddSecondTick;
+        }
+
+        #endregion
+    }
+}
